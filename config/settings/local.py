8cb15d4cfd20bce37eda1da4f1f081546f2a258f
antonio <marcosmeza01@outlook.com>
2025-06-21 22:10:43 -0600

    deploy dev

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,7 +7,9 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = True
+#DEBUG = True
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env(
     "DJANGO_SECRET_KEY",
