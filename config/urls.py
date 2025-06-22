9d02ef52c814227d2bcbced0f3b728711a2962a7
antonio <marcosmeza01@outlook.com>
2025-06-21 22:37:03 -0600

    deploy dev

--- config/urls.py
+++ config/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += [
         path(
             "400/",
